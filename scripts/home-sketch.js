462f7c9e05847d388c05076b23a5234a18ad716d
Noah Schwadron <noah.schwadron@gmail.com>
2024-01-05 14:49:13 -0500

    converted to a jekyll site

---
+++ scripts/home-sketch.js
@@ -0,0 +1,97 @@
+var planets = [];
+var numStories = 16;
+var domElements = document.getElementById("domElements")
+var randomPlanetDescription = document.getElementById("randomPlanetDescription")
+const menu = document.getElementById("planets-menu")
+var mouseOverDiamond = 0
+
+var speed;
+
+var spaceDimensions = {
+    x: 6000,
+    y: 6000,
+    z: 8000000
+  };
+
+let visibleDist = spaceDimensions.z * 0.05
+
+let cameraCoords = {
+    x: spaceDimensions.x/2,
+    y: spaceDimensions.y/2,
+    z: 0
+  };
+
+
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('myCanvasContainer');
+    angleMode(DEGREES);
+    for (var i = 0; i < 6000; i++) {
+        planets[i] = new Planet(i, 6000);
+    }
+    // place stories on random planets
+    for (let i = 0; i < numStories; i++) {
+      let rand = Math.floor(Math.random() * planets.length-1000) + 1000;
+      let hoverContainer = document.createElement("div");
+      let menuPlanet = document.createElement("div");
+      menuPlanet.classList.add('menuPlanetMarker')
+      menu.appendChild(menuPlanet);
+      planets[rand].description = document.createElement("div");
+      domElements.appendChild(hoverContainer);
+      planets[rand].mark = document.createElement("div");
+      let x = random()*spaceDimensions.x*2 - spaceDimensions.y*1
+      let y = random()*spaceDimensions.y*2 - spaceDimensions.y*1
+      planets[rand].x = x
+      planets[rand].y = y
+      planets[rand].mark.classList.add("planetMarker");
+      planets[rand].description.classList.add("planetDescription")
+      hoverContainer.appendChild(planets[rand].mark);
+      hoverContainer.appendChild(planets[rand].description);
+      planets[rand].markOffset = [random()-.5, random()-.5]
+      // planets[rand].markOffset = [random()*1000-500, random()*1000-500]
+      
+  }
+  
+    rectMode(CENTER); // Draw the rectangle from its center
+}
+let isAnyPlanetHovered = false;
+
+function draw() {
+    isAnyPlanetHovered = 0;
+    background(color('#E6FFFF'))
+    fill('#E6FFFF')
+    stroke(color('#0000E9'))
+
+    translate(width / 2, height / 2);
+    // rotate(45); // Rotate by 45 degrees
+
+    for (var i = 0; i < planets.length; i++) {
+      planets[i].show();
+    }
+    if (!isAnyPlanetHovered) {
+      document.getElementById("randomPlanetDescription").style.display = "none";
+    }
+}
+
+
+
+// window.addEventListener('scroll', () => {
+//     cameraCoords.z = map(window.scrollY, 0, height, 0, spaceDimensions.z*.8)
+//   });
+
+
+const slider = document.getElementById('mySlider');
+
+slider.oninput = function() {
+    cameraCoords.z = map(this.value, 1, 100, 0, spaceDimensions.z-visibleDist)
+}
+
+
+
+
+
+
+
+
+
---
+++ scripts/planet.js
@@ -0,0 +1,79 @@
+
+
+function Planet(depth, numPlanets) {
+    this.x = random(-spaceDimensions.x*10, spaceDimensions.x*10);
+    this.y = random(-spaceDimensions.y*10, spaceDimensions.y*10);
+    this.z = map(depth, numPlanets, 0, 0, spaceDimensions.z);
+    this.pz = this.z;
+    this.mark;
+    this.markOffset;
+    this.description;
+    this.r;
+  
+    // this.update = function() {
+    //   this.z = this.z - speed;
+    //   if (this.z < 1) {
+    //     this.z = width;
+    //     this.x = random(-width, width);
+    //     this.y = random(-height, height);
+    //     this.pz = this.z;
+    //   }
+    // }
+  
+    this.show = function() {
+    fill('#E6FFFF')
+    let dist = this.z - cameraCoords.z;
+
+        if (this.z > cameraCoords.z && dist < visibleDist ) {
+            var sx = map(this.x / dist, 0, 1, 0, spaceDimensions.x);
+            var sy = map(this.y / dist, 0, 1, 0, spaceDimensions.y);
+            this.r = map(dist, 0, visibleDist, 100, 4);
+            if (this.mark) {
+                this.mark.style.display = "block"
+                this.mark.style.left = sx+width/2+this.markOffset[0]*this.r + "px";
+                this.mark.style.top = sy+height/2+this.markOffset[1]*this.r + "px";
+                this.description.style.left = sx+width/2+10 + "px";
+                this.description.style.top = sy+height/2+10 + "px";
+            }
+            if (isMouseOverDiamond(sx + width / 2, sy + height / 2, this.r)) {
+                randomPlanetDescription.style.display = "block";
+                randomPlanetDescription.style.left = sx+width/2+this.r*.8+5 + "px";
+                randomPlanetDescription.style.top = sy+height/2 + "px";
+                isAnyPlanetHovered +=1;
+                fill("#E7D5FF")
+            }
+            drawDiamond(sx, sy, this.r);
+
+        } else {
+            if (this.mark) {this.mark.style.display = "none"}
+        }
+    }
+}
+
+
+
+function drawDiamond(x, y, size) {
+    let diamondSize = size * sqrt(2); // Increase size to match the area of the square
+
+    // Calculate the diamond's four corner points
+    let top = createVector(x, y - diamondSize / 2);
+    let right = createVector(x + diamondSize / 2, y);
+    let bottom = createVector(x, y + diamondSize / 2);
+    let left = createVector(x - diamondSize / 2, y);
+
+    // Draw the diamond
+    beginShape();
+    vertex(top.x, top.y);
+    vertex(right.x, right.y);
+    vertex(bottom.x, bottom.y);
+    vertex(left.x, left.y);
+    endShape(CLOSE);
+}
+
+
+function isMouseOverDiamond(x, y, size) {
+    let diamondSize = size * sqrt(2);
+    return mouseX > x - diamondSize / 2 && mouseX < x + diamondSize / 2 &&
+           mouseY > y - diamondSize / 2 && mouseY < y + diamondSize / 2;
+
+}
